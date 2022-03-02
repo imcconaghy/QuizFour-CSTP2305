000a316fa4290727170c9aaa6702b7e1752f94c6
Ian McConaghy <ian.mcconaghy@student.vcc.ca>
2022-03-02 11:37:29 -0800

    quizFour files

---
+++ quizFour/quizFour/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  quizFour
+//
+//  Created by Ian McConaghy on 2022-03-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        quizFourApp()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ quizFour/quizFour/quizFourApp.swift
@@ -0,0 +1,26 @@
+//
+//  quizFourApp.swift
+//  quizFour
+//
+//  Created by Ian McConaghy on 2022-03-02.
+//
+
+import SwiftUI
+
+struct quizFourApp: View {
+    var body: some View {
+        ZStack {
+            VStack{
+                ignoresSafeArea(.all)
+                    .foregroundColor(.purple)
+                
+                HStack {
+                    
+                }
+                
+            }
+            .padding()
+            
+        }
+    }
+}
